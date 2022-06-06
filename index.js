52fc14bb1123cf53fd8583fe2b9027c2529ef556
rgioeli <rgioeli@icloud.com>
2022-06-06 11:52:23 -0400

    changed port

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import { readOrderFile } from "./readOrderFile.js";
 
 const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.post("/api/customers", (req, res) => {
-    console.log(req.body)
-    return res.json(req.body)
-})
+  console.log(req.body);
+  return res.json(req.body);
+});
 
 app.get("/api/test", (req, res) => {
-    const data = readOrderFile("./info.txt");
-    return res.send(data.map(x => x.price))
-})
+  const data = readOrderFile("./info.txt");
+  return res.send(data.map((x) => x.price));
+});
 
-app.listen(8000 || process.env.port, () => console.log("Let's challenge ourselves!"))
+app.listen(process.env.port || 8000, () =>
+  console.log("Let's challenge ourselves!")
+);
